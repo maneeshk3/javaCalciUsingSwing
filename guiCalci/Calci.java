07ccc5ab35f31b8cd0d41f005791a5ae17ce2a65
Maneesh Kumar <maneeshkumar@apples-MacBook-Pro.local>
2023-05-30 18:42:17 +0530

    revamped calci

--- guiCalci/Calci.java
+++ guiCalci/Calci.java
@@ -1,70 +1,95 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-import java.awt.event.ActionListener;
+public class Calci implements ActionListener{
+    JFrame frame;
+    JTextField txtFld;
+    JButton numBut[] = new JButton[10];
+    JButton subBut, addBut, mulBut, divBut, clrBut, decBut, eqlBut, delBut;
 
-class Calci extends JFrame{
+    JPanel panel;
 
-    public Calci(){
-        setVisible(true);
-        setSize(200, 400);
+    Calci(){  
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 500);
+        frame.setLayout(null);
 
-        //creating a text field.
-        JTextField textfield = new JTextField(10);
-
-        //creating a pannnel
-        JPanel panarea = new JPanel();
-
-        // create number buttons        
-        JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, badd, bsub, bdiv, bmul, bdot, bcan, beql;
-        //creating numberic button.
-        b0 = new JButton("0");
-        b1 = new JButton("1");
-        b2 = new JButton("2");
-        b3 = new JButton("3");
-        b4 = new JButton("4");
-        b5 = new JButton("5");
-        b6 = new JButton("6");
-        b7 = new JButton("7");
-        b8 = new JButton("8");
-        b9 = new JButton("9");
-        // equals button
-        beql = new JButton("=");
-        // create operator buttons
-        badd = new JButton("+");
-        bsub = new JButton("-");
-        bdiv = new JButton("/");
-        bmul = new JButton("*");
-        bcan = new JButton("C");
-        // create . button
-        bdot = new JButton(".");
+        txtFld = new JTextField();
+        txtFld.setBounds(50, 25, 300, 50);
+        txtFld.setEditable(false);
         
-        //adding components to the fpanel.
-        panarea.add(textfield);
-        panarea.add(b0);
-        panarea.add(b1);
-        panarea.add(b2);
-        panarea.add(b3);
-        panarea.add(b4);
-        panarea.add(b5);
-        panarea.add(b6);
-        panarea.add(b7);
-        panarea.add(b8);
-        panarea.add(b9);
-        panarea.add(badd);
-        panarea.add(bsub);
-        panarea.add(bdiv);
-        panarea.add(bmul);
-        panarea.add(beql);
-        panarea.add(bdot);
-        panarea.add(bcan);
+        subBut = new JButton("-");
+        addBut = new JButton("+");
+        mulBut = new JButton("*");
+        divBut = new JButton("/");
+        clrBut = new JButton("C");
+        decBut = new JButton(".");
+        eqlBut = new JButton("=");
+        delBut = new JButton("<-");
 
-        //adding panel to the frame
-        this.getContentPane().add(panarea);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JButton funBut[]= {subBut, addBut, mulBut, divBut, clrBut, decBut, eqlBut, delBut};
+        for(int i = 0 ; i < 8 ; i++){
+            funBut[i].addActionListener(this);
+            funBut[i].setFocusable(false);
+        }
+
+        for ( int i = 0 ; i < 10 ; i++){
+            numBut[i] = new JButton(String.valueOf(i));
+            numBut[i].addActionListener(this);
+            numBut[i].setFocusable(false);
+        }
+        
+        //creating button size to add in seperately 
+        clrBut.setBounds(80, 390, 80, 65);
+        delBut.setBounds(240, 390, 80, 65);
+        
+        //creating a panel for adding button in it.
+        panel = new JPanel();
+        panel.setBounds(25, 90, 350, 290);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+
+        //adding button to the panel
+        panel.add(addBut);
+        panel.add(subBut);
+        panel.add(mulBut);
+        panel.add(divBut);
+
+        panel.add(numBut[1]);
+        panel.add(numBut[2]);
+        panel.add(numBut[3]);
+        panel.add(numBut[4]);
+        panel.add(numBut[5]);
+        panel.add(numBut[6]);
+        panel.add(numBut[7]);
+        panel.add(numBut[8]);
+
+        panel.add(decBut);
+        
+        panel.add(numBut[9]);
+        panel.add(numBut[0]);
+        
+        panel.add(eqlBut);
+
+
+        frame.add(panel);
+        frame.add(clrBut);
+        frame.add(delBut);
+        frame.add(txtFld);
+        frame.setVisible(true);
 
     }
+  
 
     public static void main(String[] args) {
         Calci calculator = new Calci();
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
 }
+
